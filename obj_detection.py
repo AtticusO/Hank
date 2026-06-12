86832515bebb00eb72db1782687f022baaf7c8bf
AtticusO <aporsagh@gmail.com>
2026-06-12 17:10:38 -0400

    edited obj_detection to return cords and tags

--- obj_detection.py
+++ obj_detection.py
@@ -11,8 +11,10 @@ class detect:
 
     def detection(self, frame):
         results = model.track(frame, persist=True)
+        tags = [results.names[int(cls.item())] for cls in results.boxes.cls]
+        cords = results.boxes.xyxy
         annotated_frame = results[0].plot()
-        return annotated_frame
+        return annotated_frame, tags, cords
 
 
 
